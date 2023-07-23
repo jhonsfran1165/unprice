140c310cc6004bb7f2adff027780f8e769bdc06b
Sebastian Franco <jhoan.franco@correounivalle.edu.co>
2023-07-23 19:02:12 +0200

    fix: next issues with f

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,12 @@ const nextConfig = {
       "images.unsplash.com",
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false
+    }
+    return config
+  },
 }
 
 export default nextConfig
