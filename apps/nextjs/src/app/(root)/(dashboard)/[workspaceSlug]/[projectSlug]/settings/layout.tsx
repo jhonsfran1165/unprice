7620654938a60e324dfa6bfc648d21ad8a3f4098
Sebastian Franco <jhoan.franco@correounivalle.edu.co>
2023-11-01 16:04:43 +0100

    chore: testing edge

--- apps/nextjs/src/app/(root)/(dashboard)/[workspaceSlug]/[projectSlug]/settings/layout.tsx
+++ apps/nextjs/src/app/(root)/(dashboard)/[workspaceSlug]/[projectSlug]/settings/layout.tsx
@@ -1,6 +1,16 @@
+import { DashboardShell } from "~/components/dashboard-shell"
+
 export default function ProjectSettingsLayout(props: {
   children: React.ReactNode
   params: { workspaceSlug: string }
 }) {
-  return props.children
+  return (
+    <DashboardShell
+      title="General Settings"
+      module="project"
+      submodule="settings"
+    >
+      {props.children}
+    </DashboardShell>
+  )
 }
--- apps/nextjs/src/app/(root)/(dashboard)/layout.tsx
+++ apps/nextjs/src/app/(root)/(dashboard)/layout.tsx
@@ -1,20 +1,24 @@
 // import { LegendStateHandler } from "~/components/state-handler"
 
+import DotPattern from "~/components/dot-pattern"
+import { SyncActiveOrgFromUrl } from "~/components/sync-active-org-from-url"
+import { ReactQueryProvider } from "~/trpc/provider"
+
 export default function DashboardLayout(props: {
   children: React.ReactNode
   params: { workspaceSlug: string; projectSlug: string }
 }) {
   return (
-    // <ReactQueryProvider>
-    <div className="flex min-h-screen flex-col">
-      {/* <SyncActiveOrgFromUrl />
+    <ReactQueryProvider>
+      <div className="flex min-h-screen flex-col">
+        <SyncActiveOrgFromUrl />
         <DotPattern width={40} height={40} x={-1} y={-1} />
-        <Header /> */}
-      <div className="flex grow flex-col">
-        <main className="flex flex-1 flex-col">{props.children}</main>
+        {/* <Header /> */}
+        <div className="flex grow flex-col">
+          <main className="flex flex-1 flex-col">{props.children}</main>
+        </div>
+        {/* <Footer /> */}
       </div>
-      {/* <Footer /> */}
-    </div>
-    // </ReactQueryProvider>
+    </ReactQueryProvider>
   )
 }
