e30b437e6229d9790815e3303a35935c33910616
Sebastian Franco <jhoan.franco@correounivalle.edu.co>
2023-12-10 02:17:01 +0100

    style: format

--- apps/nextjs/src/mdx-components.tsx
+++ apps/nextjs/src/mdx-components.tsx
@@ -7,25 +7,25 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     // Allows customizing built-in components, e.g. to add styling.
     h1: (props) => (
-      <h1 className="font-primary mt-10 scroll-m-20 text-4xl" {...props}>
+      <h1 className="mt-10 scroll-m-20 font-primary text-4xl" {...props}>
         {props.children}
       </h1>
     ),
     h2: (props) => (
       <h2
-        className="font-primary mt-10 scroll-m-20 border-b pb-2 text-3xl first:mt-0"
+        className="mt-10 scroll-m-20 border-b pb-2 font-primary text-3xl first:mt-0"
         {...props}
       >
         {props.children}
       </h2>
     ),
     h3: (props) => (
-      <h3 className="font-primary mt-8 scroll-m-20 text-2xl" {...props}>
+      <h3 className="mt-8 scroll-m-20 font-primary text-2xl" {...props}>
         {props.children}
       </h3>
     ),
     h4: (props) => (
-      <h4 className="font-primary -mb-4 mt-6 scroll-m-20 text-2xl" {...props}>
+      <h4 className="-mb-4 mt-6 scroll-m-20 font-primary text-2xl" {...props}>
         {props.children}
       </h4>
     ),
