88e29a59084ad5b4d53bcfc3e3380f5a8d93fad4
Sebastian Franco <jhoan.franco@correounivalle.edu.co>
2024-03-04 18:19:16 +0100

    fix: ci

--- apps/nextjs/src/lib/domains.ts
+++ apps/nextjs/src/lib/domains.ts
@@ -1,20 +1,20 @@
 export const getSubdomain = (name: string, apexName: string) => {
-  if (name === apexName) return null;
-  return name.slice(0, name.length - apexName.length - 1);
-};
+  if (name === apexName) return null
+  return name.slice(0, name.length - apexName.length - 1)
+}
 
 export const getApexDomain = (url: string) => {
-  let domain;
+  let domain
   try {
-    domain = new URL(url).hostname;
+    domain = new URL(url).hostname
   } catch (e) {
-    return "";
+    return ""
   }
-  const parts = domain.split(".");
+  const parts = domain.split(".")
   if (parts.length > 2) {
     // if it's a subdomain (e.g. dub.vercel.app), return the last 2 parts
-    return parts.slice(-2).join(".");
+    return parts.slice(-2).join(".")
   }
   // if it's a normal domain (e.g. dub.sh), we return the domain
-  return domain;
-};
+  return domain
+}
