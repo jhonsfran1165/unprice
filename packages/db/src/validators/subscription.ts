de478a3c72f780942b9c03fd6b512e14ac96f032
Sebastian Franco <jhoan.franco@correounivalle.edu.co>
2024-05-23 19:30:11 +0200

    chore: add some validations to items subscriptions

--- packages/db/src/validators/subscription.ts
+++ packages/db/src/validators/subscription.ts
@@ -17,12 +17,43 @@ const itemSchema = z.object({
 })
 
 // stripe won't allow more than 250 items in a single invoice
-export const subscriptionItemsSchema = z.array(itemSchema).refine((items) => {
-  if (items.length > 250) {
-    return false
-  }
-  return true
-}, "Total items for the subscription should be less than 250")
+export const subscriptionItemsSchema = z
+  .array(itemSchema)
+  .superRefine((items, ctx) => {
+    if (items.length > 250) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Total items for the subscription should be less than 250",
+        path: [0, "quantity"],
+        fatal: true,
+      })
+
+      return false
+    }
+
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i]
+
+      if (item?.quantity && item.limit && item.quantity > item.limit) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "Quantity is required for feature items",
+          path: [i, "quantity"],
+          fatal: true,
+        })
+
+        return false
+      }
+    }
+
+    return true
+  })
+  .refine((items) => {
+    if (items.length > 250) {
+      return false
+    }
+    return true
+  }, "Total items for the subscription should be less than 250")
 
 export const subscriptionSelectSchema = createSelectSchema(subscriptions, {
   metadata: subscriptionMetadataSchema,
