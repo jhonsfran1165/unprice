c07e6bb368b352cb145b68d3758ebcd565a4e2ad
Sebastian Franco <jhonsfran@gmail.com>
2024-12-27 16:18:43 +0100

    chore: prod pipeline 2

--- knip.ts
+++ knip.ts
@@ -2,6 +2,7 @@ import type { KnipConfig } from "knip"
 
 const config: KnipConfig = {
   ignoreDependencies: ["cz-conventional-changelog"],
+  project: ["apps/**/*.{js,ts}"],
 }
 
 export default config
