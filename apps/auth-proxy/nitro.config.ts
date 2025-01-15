d79fb598a9000539fda5cbe8f1b8c183500f7902
Sebastian Franco <jhonsfran@gmail.com>
2025-01-15 19:01:33 +0100

    fix: auth proxy

--- apps/auth-proxy/nitro.config.ts
+++ apps/auth-proxy/nitro.config.ts
@@ -3,7 +3,7 @@ import { defineNitroConfig } from "nitropack/config"
 export default defineNitroConfig({
   compatibilityDate: "2025-01-10",
   output: {
-    dir: ".output",
-    publicDir: ".output/public",
+    dir: ".vercel",
+    publicDir: ".vercel/public",
   },
 })
