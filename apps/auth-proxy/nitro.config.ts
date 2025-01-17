5da00fba9293902ebf9eee509fbcaf82df979845
Sebastian Franco <jhonsfran@gmail.com>
2025-01-17 21:34:47 +0100

    fix(ci): pipeline ignore 2

--- apps/auth-proxy/nitro.config.ts
+++ apps/auth-proxy/nitro.config.ts
@@ -3,8 +3,4 @@ import { defineNitroConfig } from "nitropack/config"
 export default defineNitroConfig({
   compatibilityDate: "2025-01-10",
   preset: "vercel-edge",
-  output: {
-    dir: ".output",
-    publicDir: ".output/public",
-  },
 })
