bb3a1da583a5e205da1ceb02b392aaef00774350
Sebastian Franco <jhonsfran@gmail.com>
2025-01-18 12:21:59 +0100

    chore: debug 8

--- internal/config/src/env.mjs
+++ internal/config/src/env.mjs
@@ -9,7 +9,7 @@ export const env = createEnv({
   server: {},
   client: {
     NEXT_PUBLIC_APP_DOMAIN: z.string().optional().default("localhost:3000"),
-    NEXT_PUBLIC_VERCEL_ENV: z.enum(["development", "preview", "production"]),
+    NEXT_PUBLIC_VERCEL_ENV: z.enum(["development", "preview", "production"]).optional(),
   },
   // Client side variables gets destructured here due to Next.js static analysis
   // Shared ones are also included here for good measure since the behavior has been inconsistent
