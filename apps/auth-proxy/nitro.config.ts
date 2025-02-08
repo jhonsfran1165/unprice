f982fb672f949fa8d611d19bfdefd11226237bee
Sebastian Franco <jhonsfran@gmail.com>
2025-02-08 13:33:43 +0100

    chore: define output dir

--- apps/auth-proxy/nitro.config.ts
+++ apps/auth-proxy/nitro.config.ts
@@ -3,4 +3,7 @@ import { defineNitroConfig } from "nitropack/config"
 export default defineNitroConfig({
   compatibilityDate: "2025-01-10",
   preset: "vercel-edge",
+  output: {
+    dir: "public",
+  },
 })
