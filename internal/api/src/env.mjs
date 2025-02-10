9aa9cbc96cef16f1e4c5464271b90fca34065ce6
Sebastian Franco <jhonsfran@gmail.com>
2025-02-10 11:34:38 +0100

    chore: vercel deployment 5

--- internal/api/src/env.mjs
+++ internal/api/src/env.mjs
@@ -1,6 +1,8 @@
 import { createEnv } from "@t3-oss/env-nextjs"
 import * as z from "zod"
 
+console.info("ENV", process.env)
+
 export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
