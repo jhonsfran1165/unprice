c8e55aaf915edc8879aaf7fbc1f7c83a6571cb24
Sebastian Franco <jhonsfran@gmail.com>
2025-02-12 22:21:15 +0100

    fix: tb action 2

--- internal/tinybird/scripts/append_fixtures.sh
+++ internal/tinybird/scripts/append_fixtures.sh
@@ -1,6 +1,5 @@
-
 #!/usr/bin/env bash
-set -euxo pipefail
+set -e
 
 VERSION=$1
 
@@ -12,12 +11,16 @@ absolute_directory=$(realpath "$directory")
 for extension in "${extensions[@]}"; do
   file_list=$(find "$absolute_directory" -type f -name "*.$extension")
 
-  for file_path in $file_list; do
-    file_name=$(basename "$file_path")
-    file_name_without_extension="${file_name%.*}"
+  if [ -z "$file_list" ]; then
+    echo "⚠️ Warning: No files with .$extension extension found in $absolute_directory"
+  else
+    for file_path in $file_list; do
+      file_name=$(basename "$file_path")
+      file_name_without_extension="${file_name%.*}"
 
-    command="tb --semver $VERSION datasource append $file_name_without_extension datasources/fixtures/$file_name"
-    echo $command
-    $command
-  done
+      command="tb datasource append $file_name_without_extension datasources/fixtures/$file_name"
+      echo $command
+      $command
+    done
+  fi
 done
--- internal/tinybird/scripts/exec_test.sh
+++ internal/tinybird/scripts/exec_test.sh
@@ -1,17 +1,11 @@
-
 #!/usr/bin/env bash
 set -euxo pipefail
 
 export TB_VERSION_WARNING=0
-export VERSION=$1
 
 run_test() {
     t=$1
     echo "** Running $t **"
-    # Check if VERSION is provided
-    if [[ -n $VERSION ]]; then
-        sed -i "s/tb/tb --semver $VERSION/" $t
-    fi
     echo "** $(cat $t)"
     tmpfile=$(mktemp)
     retries=0
@@ -56,7 +50,7 @@ run_test() {
 export -f run_test
 
 fail=0
-find ./tests -name "*.test" -print0 | xargs -0 -I {} -P 4 bash -c 'run_test "$@"' _ {} $VERSION || fail=1
+find ./tests -name "*.test" -print0 | xargs -0 -I {} -P 4 bash -c 'run_test "$@"' _ {} || fail=1
 
 if [ $fail == 1 ]; then
   exit -1;
