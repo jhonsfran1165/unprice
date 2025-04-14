35b274c4d354766c4f1973c83d2b441796f89009
Sebastian Franco <jhonsfran@gmail.com>
2025-04-14 19:43:37 +0200

    fix: deployment 44

--- apps/api/src/middleware/init.ts
+++ apps/api/src/middleware/init.ts
@@ -1,6 +1,6 @@
 import { createConnection } from "@unprice/db"
 import { newId } from "@unprice/db/utils"
-import { ConsoleLogger } from "@unprice/logging"
+import { BaseLimeLogger, ConsoleLogger } from "@unprice/logging"
 import { CacheService } from "@unprice/services/cache"
 import { CustomerService } from "@unprice/services/customers"
 import { LogdrainMetrics, NoopMetrics } from "@unprice/services/metrics"
@@ -49,12 +49,43 @@ export function init(): MiddlewareHandler<HonoEnv> {
 
     c.res.headers.set("unprice-request-id", requestId)
 
-    // TODO: define proper logger
-    const logger = new ConsoleLogger({
-      requestId,
-      application: "api",
-      environment: c.env.NODE_ENV,
-    })
+    const logger = c.env.EMIT_METRICS_LOGS
+      ? new BaseLimeLogger({
+          apiKey: c.env.BASELIME_APIKEY,
+          requestId,
+          defaultFields: {
+            isolateId: c.get("isolateId"),
+            isolateCreatedAt: c.get("isolateCreatedAt"),
+            requestId: c.get("requestId"),
+            requestStartedAt: c.get("requestStartedAt"),
+            performanceStart: c.get("performanceStart"),
+            location: c.get("location"),
+            workspaceId: c.get("workspaceId"),
+            projectId: c.get("projectId"),
+            userAgent: c.get("userAgent"),
+          },
+          namespace: "unprice-api",
+          dataset: "unprice-api",
+          service: "api", // default service name
+          flushAfterMs: 3000, // flush after 3 secs
+          ctx: {
+            waitUntil: c.executionCtx.waitUntil.bind(c.executionCtx),
+          },
+          environment: c.env.NODE_ENV,
+          application: "api",
+        })
+      : new ConsoleLogger({
+          requestId,
+          environment: c.env.NODE_ENV,
+          application: "api",
+          defaultFields: {
+            isolateId: c.get("isolateId"),
+            isolateCreatedAt: c.get("isolateCreatedAt"),
+            requestId: c.get("requestId"),
+            requestStartedAt: c.get("requestStartedAt"),
+            performanceStart: c.get("performanceStart"),
+          },
+        })
 
     const metrics: Metrics = c.env.EMIT_METRICS_LOGS
       ? new LogdrainMetrics({
