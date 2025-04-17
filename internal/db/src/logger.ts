50845a46ac6c73e2971ccc49864efd852595b215
Sebastian Franco <jhonsfran@gmail.com>
2025-04-17 12:36:26 +0200

    fix: deployment 50

--- internal/db/src/logger.ts
+++
@@ -1,7 +0,0 @@
-import type { Logger } from "drizzle-orm/logger"
-
-export class DBLogger implements Logger {
-  logQuery(query: string, params: unknown[]): void {
-    console.info(query, params)
-  }
-}
